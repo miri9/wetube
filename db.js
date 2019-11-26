c12b81bcd06298f58cc369cfcd38a39a3da8b9d7
miri <mirimiri989@gmail.com>
2019-11-26 15:00:17 +0900

    2.21

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+	{
+		id:12234,
+		title: "Video awesome",
+		description: "This is something I love.",
+		views: 25,
+		videoFile:"https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
+		creator: {
+			id:10000,
+			name:"Miri",
+			email:"miri@miri.com"
+		}
+	},
+	{
+		id:57784,
+		title: "Video nice",
+		description: "This is something I love.",
+		views: 25,
+		videoFile:"https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
+		creator: {
+			id:10000,
+			name:"Miri",
+			email:"miri@miri.com"
+		}
+	},
+	{
+		id:99802,
+		title: "Video cool",
+		description: "This is something I love.",
+		views: 25,
+		videoFile:"https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
+		creator: {
+			id:10000,
+			name:"Miri",
+			email:"miri@miri.com"
+		}
+	},
+	{
+		id:35519,
+		title: "Video perfect",
+		description: "This is something I love.",
+		views: 25,
+		videoFile:"https://interactive-examples.mdn.mozilla.net/media/examples/flower.webm",
+		creator: {
+			id:10000,
+			name:"Miri",
+			email:"miri@miri.com"
+		}
+	},
+]
