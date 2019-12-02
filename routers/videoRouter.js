351c7f1703142833e7e55b72723b6b5c7103e952
miri <mirimiri989@gmail.com>
2019-12-02 19:06:32 +0900

    3.9

--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,14 +1,19 @@
 import express from "express";
 import routes from "../routes";
-import { videos, getUpload, postUpload, videoDetail, editVideo, deleteVideo } from "../controllers/videoController";
+import { videos, getUpload, postUpload, videoDetail, getEditVideo, postEditVideo, deleteVideo } from "../controllers/videoController";
 import { uploadVideo } from "../middlewares.js";
 
 const videoRouter = express.Router();
 
+// UPLOAD
 videoRouter.get(routes.upload, getUpload);
 videoRouter.post(routes.upload, uploadVideo, postUpload);
+// VIDEO DETAIL
 videoRouter.get(routes.videoDetail(), videoDetail);
-videoRouter.get(routes.editVideo, editVideo);
+// EDIT VIDEO
+videoRouter.get(routes.editVideo(), getEditVideo);
+videoRouter.post(routes.editVideo(), postEditVideo);
+//DELETE VIDEO
 videoRouter.get(routes.deleteVideo, deleteVideo);
 
 
