f2353975a64c6b9762f25b3c459ed10e45c720b8
miri <mirimiri989@gmail.com>
2019-12-25 20:09:36 +0900

    6.5

--- middlewares.js
+++ middlewares.js
@@ -7,8 +7,24 @@ export const localMiddleware = (req, res, next) => {
   res.locals.siteName = "WeTube";
   res.locals.routes = routes;
   res.locals.user = req.user || null;
-  console.log(req.user);
   next();
+};
+
+export const onlyPublic = (req, res, next) => {
+  if (req.user) {
+    res.redirect(routes.home);
+  } else {
+    next();
+  }
+};
+// 로그아웃 상태에서만 접근을 허용하겠다는 의미.
+
+export const onlyPrivate = (req, res, next) => {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect(routes.home);
+  }
 };
 
 export const uploadVideo = multerVideo.single("videoFile");
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,20 +1,26 @@
 import express from "express";
 import routes from "../routes";
-import { videos, getUpload, postUpload, videoDetail, getEditVideo, postEditVideo, deleteVideo } from "../controllers/videoController";
-import { uploadVideo } from "../middlewares.js";
+import {
+  getUpload,
+  postUpload,
+  videoDetail,
+  getEditVideo,
+  postEditVideo,
+  deleteVideo
+} from "../controllers/videoController";
+import { uploadVideo, onlyPrivate } from "../middlewares.js";
 
 const videoRouter = express.Router();
 
 // UPLOAD
-videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, uploadVideo, postUpload);
+videoRouter.get(routes.upload, onlyPrivate, getUpload);
+videoRouter.post(routes.upload, onlyPrivate, uploadVideo, postUpload);
 // VIDEO DETAIL
 videoRouter.get(routes.videoDetail(), videoDetail);
 // EDIT VIDEO
-videoRouter.get(routes.editVideo(), getEditVideo);
-videoRouter.post(routes.editVideo(), postEditVideo);
+videoRouter.get(routes.editVideo(), onlyPrivate, getEditVideo);
+videoRouter.post(routes.editVideo(), onlyPrivate, postEditVideo);
 //DELETE VIDEO
-videoRouter.get(routes.deleteVideo(), deleteVideo);
-
+videoRouter.get(routes.deleteVideo(), onlyPrivate, deleteVideo);
 
 export default videoRouter;
