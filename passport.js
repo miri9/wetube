7ad21d0f5f818d324fe96965ccebc8f6cca5d20b
miri <mirimiri989@gmail.com>
2019-12-28 03:12:10 +0900

    6.11

--- passport.js
+++ passport.js
@@ -28,7 +28,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: "http://localhost:4000/auth/facebook/callback"
+      callbackURL: `https://35dc2f7f.ngrok.io${routes.facebookCallback}`,
+      profileFields: ["id", "displayName", "photos", "email"],
+      scope: ["public_profile", "email"]
     },
     facebookLoginCallback
   )
