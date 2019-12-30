6979e36a613543d85fab0a0b2f10df754f934b78
miri <mirimiri989@gmail.com>
2019-12-31 02:03:49 +0900

    8.4

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -3,23 +3,79 @@ const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayBtn");
 const volumeBtn = document.getElementById("jsVolumeBtn");
 const fullScrnBtn = document.getElementById("jsFullScreen");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
+
+function setTotalTime() {
+  const totalTimeString = formatDate(videoPlayer.duration);
+  totalTime.innerHTML = totalTimeString;
+  setInterval(getCurrentTime, 1000);
+}
+
+function getCurrentTime() {
+  const currentTimeString = formatDate(videoPlayer.currentTime);
+  currentTime.innerHTML = currentTimeString;
+}
 
 function init() {
   playBtn.addEventListener("click", handlePlayClick);
   videoPlayer.addEventListener("click", handlePlayClick);
+  videoPlayer.addEventListener("loadedmetadata", setTotalTime);
   volumeBtn.addEventListener("click", handleVolumeClick);
   fullScrnBtn.addEventListener("click", goFullScreen);
 }
 
+const formatDate = seconds => {
+  const secondsNumber = parseInt(seconds, 10);
+  let hours = Math.floor(secondsNumber / 3600);
+  let minutes = Math.floor((secondsNumber - hours * 3600) / 60);
+  let totalSeconds = secondsNumber - hours * 3600 - minutes * 60;
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    totalSeconds = `0${totalSeconds}`;
+  }
+  return `${hours}:${minutes}:${totalSeconds}`;
+};
+
+function prefixExitFullScreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+}
+
+function prefixGoFullScreen() {
+  if (videoContainer.requestFullscreen) {
+    videoContainer.requestFullscreen();
+  } else if (videoContainer.mozRequestFullScreen) {
+    videoContainer.mozRequestFullScreen();
+  } else if (videoContainer.webkitRequestFullscreen) {
+    videoContainer.webkitRequestFullscreen();
+  } else if (videoContainer.msRequestFullscreen) {
+    videoContainer.msRequestFullscreen();
+  }
+}
+
 function exitFullScreen() {
-  document.exitFullscreen();
+  prefixExitFullScreen();
   fullScrnBtn.innerHTML = '<i class="fas fa-expand"></i>';
   fullScrnBtn.removeEventListener("click", exitFullScreen);
   fullScrnBtn.addEventListener("click", goFullScreen);
 }
 
 function goFullScreen() {
-  videoContainer.webkitRequestFullscreen();
+  prefixGoFullScreen();
   fullScrnBtn.innerHTML = '<i class="fas fa-compress"></i>';
   fullScrnBtn.removeEventListener("click", goFullScreen);
   fullScrnBtn.addEventListener("click", exitFullScreen);
--- db.js
+++ db.js
@@ -4,21 +4,20 @@ import dotenv from "dotenv";
 dotenv.config();
 // .env 파일 안의 정보 불러옴. 그리고 찾은 모든 변수들을 process.env.key 에 저장할 것임.
 
-mongoose.connect(process.env.MONGO_URL,
-	{
-		useNewUrlParser: true,
-		useFindAndModify: false
-	}
-);
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
 
 const db = mongoose.connection;
 
 const handleOpen = () => {
-	console.log("Connected to DB!!");
-} 
-const handleError = (error) => {
-	console.log(`X : Error on DB Connection: ${error}`);
-}
+  console.log("Connected to DB!!");
+};
+const handleError = error => {
+  console.log(`X : Error on DB Connection: ${error}`);
+};
 
 db.once("open", handleOpen);
 db.on("error", handleError);
