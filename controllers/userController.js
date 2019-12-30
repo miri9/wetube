72a1ef079947fb0cd0c38a01b3e1cfc6ef2c7199
miri <mirimiri989@gmail.com>
2019-12-30 19:29:41 +0900

    8.0

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,6 @@ import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
 
-
 // join
 
 export const getJoin = (req, res) => {
@@ -133,7 +132,7 @@ export const userDetail = async (req, res) => {
     params: { id }
   } = req;
   try {
-    const user = await User.fineById(id).populate('videos');
+    const user = await User.fineById(id).populate("videos");
     console.log(user);
     res.render("userDetail", { pageTitle: "User Detail", user });
   } catch (error) {
@@ -143,7 +142,7 @@ export const userDetail = async (req, res) => {
 
 export const getEditProfile = (req, res) => {
   res.render("editProfile", { pageTitle: "Edit Profile" });
-}
+};
 export const postEditProfile = async (req, res) => {
   const {
     body: { name, email },
@@ -162,23 +161,23 @@ export const postEditProfile = async (req, res) => {
 };
 
 export const getChangePassword = (req, res) => {
-res.render("changePassword", { pageTitle: "Change Password" });
-}
+  res.render("changePassword", { pageTitle: "Change Password" });
+};
 
 export const postChangePassword = async (req, res) => {
   const {
     body: { oldPassword, newPassword, newPassword1 }
-   } = req;
-    try {
-      if(newPassword !== newPassword1){
-        res.status(400);
-        res.redirect(`/users/${routes.changePassword}`);
-        return;
-      }
-      req.user.changePassword(oldPassword, newPassword, cb);
-      res.redirect(routes.me);
-    }catch(error){
-        res.status(400);
+  } = req;
+  try {
+    if (newPassword !== newPassword1) {
+      res.status(400);
       res.redirect(`/users/${routes.changePassword}`);
+      return;
     }
+    req.user.changePassword(oldPassword, newPassword);
+    res.redirect(routes.me);
+  } catch (error) {
+    res.status(400);
+    res.redirect(`/users/${routes.changePassword}`);
   }
+};
