31a2761a46418870f03cab42f9028cdf4f264e2d
miri <mirimiri989@gmail.com>
2020-01-02 17:40:39 +0900

    9.1

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -2,23 +2,40 @@ const recordContainer = document.getElementById("jsRecordContainer");
 const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
-const startRecording = async () => {
+let streamObject;
+
+const handleVideoData = event => {
+  console.log(event);
+};
+
+const startRecording = () => {
+  const videoRecorder = new MediaRecorder(streamObject);
+  videoRecorder.addEventListener = ("dataavailable",handleVideoData);
+  videoRecorder.start();
+  console.log(videoRecorder);
+};
+
+const getVideo = async () => {
   try {
     const stream = await navigator.mediaDevices.getUserMedia({
       audio: true,
       video: { width: 1280, height: 720 }
     });
     videoPreview.srcObject = stream;
-    videoPreview.muted();
+    videoPreview.muted = true;
     videoPreview.play();
+    recordBtn.innerHTML = "Stop Recording";
+    streamObject = stream;
+    startRecording(streamObject);
   } catch (error) {
     recordBtn.innerHTML = " :( Can't record. ";
-    recordBtn.removeEventListener("click", startRecording);
+  } finally {
+    recordBtn.removeEventListener("click", getVideo);
   }
 };
 
 function init() {
-  recordBtn.addEventListener("click", startRecording);
+  recordBtn.addEventListener("click", getVideo);
 }
 
 if (recordContainer) {
