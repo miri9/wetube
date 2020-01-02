99efc9222c2a78e477d8ed76f804b26b5ccd54b6
miri <mirimiri989@gmail.com>
2020-01-02 18:23:55 +0900

    9.2

--- assets/js/videoRecorder.js
+++ assets/js/videoRecorder.js
@@ -3,16 +3,29 @@ const recordBtn = document.getElementById("jsRecordBtn");
 const videoPreview = document.getElementById("jsVideoPreview");
 
 let streamObject;
+let videoRecorder;
 
 const handleVideoData = event => {
-  console.log(event);
+  const { data: videoFile } = event;
+  const link = document.createElement("a");
+  link.href = URL.createObjectURL(videoFile);
+  link.download = "recorded.webm";
+  document.body.appendChild(link);
+  link.click(); //faking click
+};
+
+const stopRecording = () => {
+  videoRecorder.stop();
+  recordBtn.removeEventListener("click", stopRecording);
+  recordBtn.addEventListener("click", getVideo);
+  recordBtn.innerHTML = "Start Recording";
 };
 
 const startRecording = () => {
-  const videoRecorder = new MediaRecorder(streamObject);
-  videoRecorder.addEventListener = ("dataavailable",handleVideoData);
+  videoRecorder = new MediaRecorder(streamObject);
   videoRecorder.start();
-  console.log(videoRecorder);
+  videoRecorder.addEventListener("dataavailable", handleVideoData);
+  recordBtn.addEventListener("click", stopRecording);
 };
 
 const getVideo = async () => {
