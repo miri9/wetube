2e130e40ad76cc905e962f97a18e74b41c28e305
miri <mirimiri989@gmail.com>
2020-01-05 02:11:34 +0900

    10.4

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,9 +1,26 @@
 import axios from "axios";
 const addCommentForm = document.getElementById("jsAddComment");
+const commentList = document.getElementById("jsCommentList");
+const commentNumber = document.getElementById("jsCommentNumber");
 
 function init() {
   addCommentForm.addEventListener("submit", handleSubmit);
 }
+
+const increaseNumber = () => {
+  console.log(commentNumber);
+  commentNumber.innerHTML = parseInt(commentNumber.innerHTML, 10) + 1;
+};
+
+const addComment = comment => {
+  // it's fake comment
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.innerHTML = comment;
+  li.appendChild(span);
+  commentList.prepend(li);
+  increaseNumber();
+};
 
 const sendComment = async comment => {
   const videoId = window.location.href.split("/videos/")[1];
@@ -14,7 +31,9 @@ const sendComment = async comment => {
       comment
     }
   });
-  console.log(response);
+  if (response.status === 200) {
+    addComment(comment);
+  }
 };
 
 const handleSubmit = event => {
